a516c9189af2bfd68832a3459459ff86569d09dd
Tiago Lima <lima.tiago85@hotmail.com>
2020-12-28 22:33:11 +0000

    FlightsController

---
+++ AirlineCompany.web/Controllers/FlightsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AirlineCompany.web.Data;
+using AirlineCompany.web.Data.Entities;
+
+namespace AirlineCompany.web.Controllers
+{
+    public class FlightsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FlightsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Flights
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Flights.ToListAsync());
+        }
+
+        // GET: Flights/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await _context.Flights
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return View(flight);
+        }
+
+        // GET: Flights/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Flights/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FlightNumber,Date,Hour")] Flight flight)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(flight);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(flight);
+        }
+
+        // GET: Flights/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await _context.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return View(flight);
+        }
+
+        // POST: Flights/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,FlightNumber,Date,Hour")] Flight flight)
+        {
+            if (id != flight.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(flight);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FlightExists(flight.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(flight);
+        }
+
+        // GET: Flights/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await _context.Flights
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return View(flight);
+        }
+
+        // POST: Flights/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var flight = await _context.Flights.FindAsync(id);
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FlightExists(int id)
+        {
+            return _context.Flights.Any(e => e.ID == id);
+        }
+    }
+}
